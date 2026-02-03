f63c65c5096c64754c87d735aeea458566e55c3a
hackerIP54 <bombblingm@gmail.com>
2026-02-03 19:37:06 +0100

    Refactor version check and cleanup logic

--- version.lua
+++ version.lua
@@ -1,16 +1,8 @@
 local version = "1.0.0"
 local CheckV = game.Players.LocalPlayer.PlayerGui:FindFirstChild("SoundForChat5")
- if  CheckV   then
-		
-		if game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("VersionCheck").Text ~= version then
-			game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("UpdateF").Visible = true
-		game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("Main"):Destroy()
-		script:Remove()
-		else
-		game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("UpdateF"):Destroy()
-			game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("Main").Visible = true
-		script:Remove()
-		end	
-		else 
-		script:Remove()
- end
+		if CheckV then
+		game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("VersionCheck").Text = version 
+	wait(3)
+	script:Destroy()
+end
+

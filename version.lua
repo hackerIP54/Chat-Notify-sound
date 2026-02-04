8a590c33ba4eec91a17d008ed23efb71c637c57e
hackerIP54 <bombblingm@gmail.com>
2026-02-04 07:55:23 +0100

    Add wait time before version check
    Added a wait time before checking for SoundForChat5.

--- version.lua
+++ version.lua
@@ -1,8 +1,10 @@
 local version = "1.0.0"
+
 local CheckV = game.Players.LocalPlayer.PlayerGui:FindFirstChild("SoundForChat5")
-		if CheckV then
+		wait(4)
+if CheckV then
 		game.Players.LocalPlayer.PlayerGui:WaitForChild("SoundForChat5"):WaitForChild("VersionCheck").Text = version 
 	wait(3)
-	script:Destroy()
+	
 end
 
